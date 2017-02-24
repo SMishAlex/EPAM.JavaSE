c0cfdc3bfefa663dbc42a3a8eda91053ce5bfa01
msid <sidmalex@yandex.ru>
2017-02-24 08:57:51 +0300

    create pen class, it's write now

---
+++ unit02/src/main/java/com/epam/courses/javase/msid/unit02/task1/Pen.java
@@ -0,0 +1,10 @@
+package com.epam.courses.javase.msid.unit02.task1;
+
+/**
+ * Created by msid on 24.02.17.
+ */
+public class Pen {
+    public void write(String s) {
+        System.out.print(s);
+    }
+}
