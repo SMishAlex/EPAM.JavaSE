7514dd2c03289f780059c346e7fc6ca54083a462
msid <sidmalex@yandex.ru>
2017-02-24 09:07:34 +0300

    GRIN: color and writeLine test

--- unit02/src/main/java/com/epam/courses/javase/msid/unit02/task1/Pen.java
+++ unit02/src/main/java/com/epam/courses/javase/msid/unit02/task1/Pen.java
@@ -1,10 +1,26 @@
 package com.epam.courses.javase.msid.unit02.task1;
 
+import java.awt.*;
+
 /**
  * Created by msid on 24.02.17.
  */
 public class Pen {
+    private Color color;
+
     public void write(String s) {
         System.out.print(s);
+    }
+
+    public void writeLine(String s) {
+        System.out.println(s);
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
     }
 }
