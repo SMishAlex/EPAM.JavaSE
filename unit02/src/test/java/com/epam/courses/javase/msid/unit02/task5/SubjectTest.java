a885e2f6cd67152a2b9c60a114c7cfaa49fcedb9
msid <sidmalex@yandex.ru>
2017-02-27 12:41:12 +0300

    success evaluate student test and evaluate err test

--- unit02/src/test/java/com/epam/courses/javase/msid/unit02/task5/SubjectTest.java
+++ unit02/src/test/java/com/epam/courses/javase/msid/unit02/task5/SubjectTest.java
@@ -1,6 +1,11 @@
 package com.epam.courses.javase.msid.unit02.task5;
 
+import com.epam.courses.javase.msid.unit02.task1.Pen;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.junit.Assert.*;
 
@@ -8,6 +13,7 @@ import static org.junit.Assert.*;
  * Created by msid on 27.02.17.
  */
 public class SubjectTest {
+
     @Test
     public void testAddedStudent(){
         Student student = new Student("Some Name");
@@ -15,5 +21,36 @@ public class SubjectTest {
         subject.addStudent(student);
         assertTrue(subject.getGroup().containsKey(student));
         assertTrue(student.getSubjects().contains(subject));
+    }
+
+    @Test
+    public void testEvaluateStudent() {
+        Student student = new Student("Some Name");
+        Subject<Integer> subject = new Subject<Integer>("Math", "Someone");
+        subject.addStudent(student);
+        Integer mark = new Integer(5);
+        subject.evaluateStudent(student, mark);
+        assertTrue(subject.getStudentsMarks(student).contains(mark));
+    }
+
+    @Test
+    public void testNoEvaluatedStudentInGroupException() {
+
+        Student student = new Student("Some Name");
+        Subject<Integer> subject = new Subject<Integer>("Math", "Someone");
+        subject.addStudent(student);
+        Integer mark = new Integer(5);
+
+        boolean catchException = false;
+        Student otherStudent = new Student("Some Name");
+        try {
+            subject.evaluateStudent(otherStudent, mark);
+        }
+        catch (IllegalArgumentException e){
+            catchException = true;
+            System.out.println(e);
+        }
+        assertTrue(catchException);
+
     }
 }
