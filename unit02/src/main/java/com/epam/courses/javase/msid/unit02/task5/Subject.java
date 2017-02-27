770edd712963c022e13260ae2d4de305c3658cb0
msid <sidmalex@yandex.ru>
2017-02-27 12:19:33 +0300

    change subject class to generic and saccess add student test

--- unit02/src/main/java/com/epam/courses/javase/msid/unit02/task5/Subject.java
+++ unit02/src/main/java/com/epam/courses/javase/msid/unit02/task5/Subject.java
@@ -1,23 +1,25 @@
 package com.epam.courses.javase.msid.unit02.task5;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
-public class Subject {
+public class Subject<T extends Number> {
 
     private final String title;
     private String teacher;
-    private List<Student> group;
+    Map<Student, List<T>> group;
 
     public Subject(String title, String teacher) {
         this.title = title;
         this.teacher = teacher;
-        group = new ArrayList<>();
+        group = new HashMap<Student, List<T>>();
     }
 
     public void addStudent(Student student) {
-        group.add(student);
+        group.put(student, new ArrayList<T>());
         student.addSubject(this);
     }
 
@@ -38,9 +40,27 @@ public class Subject {
         return title;
     }
 
-    public List<Student> getGroup() {
-        List<Student> dst = new ArrayList<>();
-        dst.addAll(group);
+    public Map<Student, List<T>> getGroup() {
+        HashMap<Student, List<T>> dst = new HashMap<Student, List<T>>();
+        dst.putAll(group);
         return dst;
+    }
+
+    public void evaluateStudent(Student student, T mark) {
+        studentMarks(student).add(mark);
+    }
+
+    private List<T> studentMarks(Student student) {
+        if (!group.containsKey(student))
+            throw new IllegalArgumentException("there is no such student");
+        List<T> marks = group.get(student);
+        return marks;
+    }
+
+    public List<T> getStudentsMarks(Student student) {
+        List<T> marks = studentMarks(student);
+        List dst = new ArrayList(marks);
+        return dst;
+
     }
 }
