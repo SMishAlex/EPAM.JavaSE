4b0042471c8f77e9d4780add07d366962347c65f
msid <sidmalex@yandex.ru>
2017-02-28 08:16:49 +0300

    init loger and test

---
+++ unit03/src/main/java/com/epam/courses/javase/msid/unit03/task1/CrazyLogger.java
@@ -0,0 +1,12 @@
+package com.epam.courses.javase.msid.unit03.task1;
+
+
+import java.time.Instant;
+import java.time.LocalDate;
+
+/**
+ * Created by msid on 28.02.17.
+ */
+public class CrazyLogger {
+
+}
---
+++ unit03/src/test/java/com/epam/courses/javase/msid/unit03/task1/CrazyLoggerTest.java
@@ -0,0 +1,10 @@
+package com.epam.courses.javase.msid.unit03.task1;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by msid on 28.02.17.
+ */
+public class CrazyLoggerTest {
+    
+}
