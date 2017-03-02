fc350a2d6b7486f2aaecf775de9281a3040a7a0a
msid <sidmalex@yandex.ru>
2017-03-02 14:04:04 +0300

    task2 complit

---
+++ unit03/src/test/java/com/epam/courses/javase/msid/unit03/task2/ResourceExampleTest.java
@@ -0,0 +1,18 @@
+package com.epam.courses.javase.msid.unit03.task2;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
+public class ResourceExampleTest {
+
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(System.in.read());
+    }
+    public void test() throws IOException {
+    }
+
+}
