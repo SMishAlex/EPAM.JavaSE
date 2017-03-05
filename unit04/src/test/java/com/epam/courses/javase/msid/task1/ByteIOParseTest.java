c0e8bf40fe209cb67380b947bcb0d654b68529ad
msid <sidmalex@yandex.ru>
2017-03-05 12:18:30 +0300

    GREEN: analyse himself

---
+++ unit04/src/test/java/com/epam/courses/javase/msid/task1/ByteIOParseTest.java
@@ -0,0 +1,30 @@
+package com.epam.courses.javase.msid.task1;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
+public class ByteIOParseTest {
+
+    private ByteIOParse ioParse;
+
+    @Before
+    public void testSetKeyWordsFromFile() throws IOException {
+        ioParse = new ByteIOParse();
+        ioParse.setKeyWordsFromFile("keys");
+        assertEquals(50, ioParse.getKeyWords().size());
+        assertEquals("abstract", ioParse.getKeyWords().get(0));
+    }
+
+    @Test
+    public void testFileInput() throws IOException {
+        ioParse.analyseFile("src//main//java//com//epam//courses//javase//msid//task1//ByteIOParse.java");
+        assertEquals(3, ioParse.getResult().get("try").intValue());
+    }
+
+
+}
