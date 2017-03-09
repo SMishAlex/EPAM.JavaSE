a0bf679b94480cc058d7f475eca989cf784991ec
msid <sidmalex@yandex.ru>
2017-03-09 12:54:49 +0300

    Refactoring: writing java doc

--- unit05/src/main/java/com/epam/courses/javase/msid/unit05/task2/UniversalResource.java
+++ unit05/src/main/java/com/epam/courses/javase/msid/unit05/task2/UniversalResource.java
@@ -6,6 +6,13 @@ import java.util.*;
 public class UniversalResource {
     Map<String, String> data;
 
+    /**
+     * Universal resource of some bundle with base and language.
+     *
+     * @param resource base for resource bundle
+     * @param local    An ISO 639 alpha-2 or alpha-3 language code. {@see {@link Locale#Locale(String)}}
+     * @throws NoSuchFileException
+     */
     public UniversalResource(String resource, String local) throws NoSuchFileException {
         try {
             Locale locale = new Locale(local);
@@ -19,15 +26,26 @@ public class UniversalResource {
         }
     }
 
-    public String getString(String key) {
+    /**
+     * Return string with key.
+     *
+     * @param key
+     * @return string that contains in property file with argument key
+     * @throws NoSuchElementException if there is no such key in file
+     */
+    public String getString(String key) throws NoSuchElementException {
         String result = data.get(key);
         if (result == null)
             throw new NoSuchElementException();
         return result;
     }
 
+    /**
+     * Get data hashMap.
+     *
+     * @return copy of resources.
+     */
     public Map<String, String> getData() {
         return new HashMap<>(data);
-
     }
 }
