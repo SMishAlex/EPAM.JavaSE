ebeb3efc052aca7e37345323073ca78b2d43a20f
msid <sidmalex@yandex.ru>
2017-03-09 12:08:10 +0300

    init task2 Reed tests

---
+++ unit05/src/main/java/com/epam/courses/javase/msid/unit05/task2/UniversalResource.java
@@ -0,0 +1,25 @@
+package com.epam.courses.javase.msid.unit05.task2;
+
+import java.nio.file.NoSuchFileException;
+import java.util.*;
+
+public class UniversalResource {
+    Map<String, String> data;
+
+    public UniversalResource(String resource, String local) throws NoSuchFileException {
+        try {
+            Locale locale = new Locale(local);
+            ResourceBundle bundle = ResourceBundle.getBundle(resource, locale);
+            data = new HashMap<>();
+            for (String key : bundle.keySet()) {
+                data.put(key, bundle.getString(key));
+            }
+        } catch (MissingResourceException e) {
+            throw new NoSuchFileException(resource);
+        }
+    }
+
+    public String getString(String key) {
+        return null;
+    }
+}
---
+++ unit05/src/test/java/com/epam/courses/javase/msid/unit05/task2/UniversalResourceTest.java
@@ -0,0 +1,33 @@
+package com.epam.courses.javase.msid.unit05.task2;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.nio.file.NoSuchFileException;
+import java.util.NoSuchElementException;
+
+import static org.junit.Assert.*;
+
+public class UniversalResourceTest {
+    UniversalResource resource;
+
+    @Test(expected = NoSuchFileException.class)
+    public void testNoPropertyFileException() throws NoSuchFileException {
+        UniversalResource ru = new UniversalResource("1", "ru");
+    }
+    @Before
+    public void init() throws NoSuchFileException {
+        resource = new UniversalResource("resource", "ru");
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void testNoSuchKeyException(){
+        resource.getString("no");
+    }
+
+    @Test
+    public void testThatAllPropertyHaveBeenRead(){
+        resource.getString("no");
+    }
+
+}
