c18719e30038112f511efbe76daea26b60580b8a
msid <sidmalex@yandex.ru>
2017-03-10 11:33:09 +0300

    text file class tests RED

--- unit05/src/test/java/com/epam/courses/javase/msid/unit05/task1/TextFile.java
+++ unit05/src/test/java/com/epam/courses/javase/msid/unit05/task1/TextFile.java
@@ -1,7 +1,26 @@
 package com.epam.courses.javase.msid.unit05.task1;
 
-public class TextFile {
-    public static void create(String name){
+import java.util.List;
 
+public class TextFile {
+    public TextFile(String fileName) {
+
+    }
+
+    public static void create(String name) {
+
+    }
+
+
+    public static void remove(String fileName) {
+    }
+
+    public int getLinesNumber() {
+
+        int linesNumber = 0;
+        return linesNumber;
+    }
+
+    public void writeToEnd(List<String> toWrite) {
     }
 }
--- unit05/src/test/java/com/epam/courses/javase/msid/unit05/task1/TextFileTest.java
+++ unit05/src/test/java/com/epam/courses/javase/msid/unit05/task1/TextFileTest.java
@@ -1,14 +1,50 @@
 package com.epam.courses.javase.msid.unit05.task1;
 
+import com.sun.org.apache.xerces.internal.xs.StringList;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class TextFileTest {
-    @Test
-    public void testCreatingTextFile(){
-        TextFile.create("test1");
 
+    private String fileName;
+    private FileDirectoryShower shower;
+
+    @Before
+    public void init() throws IOException {
+        fileName = "test1.txt";
+        TextFile.create(fileName);
+        shower = new FileDirectoryShower(".");
+        //assertTrue(shower.contains(fileName));
+    }
+
+    @Test
+    public void testCreatingFile() {
+        TextFile.create(fileName);
+        assertTrue("file wasn't create ", shower.contains(fileName));
+    }
+
+    @Test
+    public void testWriteToEndOfFile() {
+        TextFile textFile = new TextFile(fileName);
+        int lines = textFile.getLinesNumber();
+        List<String> toWrite = new ArrayList<>();
+        toWrite.add("some line");
+        textFile.writeToEnd(toWrite);
+        assertEquals(lines + toWrite.size(), textFile.getLinesNumber());
+    }
+
+    @After
+    public void removeTestingFiles() {
+        TextFile.remove(fileName);
+        assertFalse(shower.contains(fileName));
     }
 
 }
