c63d3867000fcec6a39d338e954e925ac04c42bc
msid <sidmalex@yandex.ru>
2017-03-11 17:13:41 +0300

    user menu and enum created

---
+++ unit05/src/main/java/com/epam/courses/javase/msid/unit05/task1/UserInterface.java
@@ -0,0 +1,80 @@
+package com.epam.courses.javase.msid.unit05.task1;
+
+import java.util.Scanner;
+
+public class UserInterface {
+    private final static Scanner scanner = new Scanner(System.in);
+    private final static String title = "This program allow to see files and directories \nof file system, create or remove txt files and write to end of file.";
+
+    private enum MenuItems {
+        SHOW_DIRECTORIES, SHOW_FILES, CREATE_TXT_FILE, WRITE_TO_TXT_FILE, REMOVE_TXT_FILE
+    }
+
+    public static void main(String[] args) {
+        showTitle();
+        while (true) {
+            showMenu();
+            askTaskAndDoSelected();
+            askToExit();
+        }
+    }
+
+    private static void showTitle() {
+        System.out.println(title);
+    }
+
+    private static void askToExit() {
+        System.out.println("wanna do something els? [y/n]");
+        if (scanner.nextLine().equalsIgnoreCase("n")) {
+            System.exit(0);
+        }
+    }
+
+    private static void showMenu() {
+        System.out.println("menu items:");
+        for (MenuItems item : MenuItems.values()) {
+            System.out.println(item.ordinal() + " " + item);
+        }
+    }
+
+    private static void askTaskAndDoSelected() {
+        int selected = Integer.parseInt(scanner.nextLine());
+        switch (MenuItems.values()[selected]) {
+            case SHOW_DIRECTORIES:
+                showDirectory();
+                break;
+            case CREATE_TXT_FILE:
+                createTxt();
+                break;
+            case REMOVE_TXT_FILE:
+                removeTxt();
+                break;
+            case SHOW_FILES:
+                showFiles();
+                break;
+            case WRITE_TO_TXT_FILE:
+                writeToTxt();
+                break;
+            default:
+                System.out.println("please select menu item");
+        }
+
+    }
+
+    private static void writeToTxt() {
+
+    }
+
+    private static void showFiles() {
+    }
+
+    private static void removeTxt() {
+    }
+
+    private static void createTxt() {
+
+    }
+
+    private static void showDirectory() {
+    }
+}
