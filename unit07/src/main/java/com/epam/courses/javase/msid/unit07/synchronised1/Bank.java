a66023077b04bfa4d17099f0df84a64debdefe5b
msid <sidmalex@yandex.ru>
2017-03-19 16:51:53 +0300

    green multythread test of bank

--- unit07/src/main/java/com/epam/courses/javase/msid/unit07/synchronised1/Bank.java
+++ unit07/src/main/java/com/epam/courses/javase/msid/unit07/synchronised1/Bank.java
@@ -1,31 +1,52 @@
 package com.epam.courses.javase.msid.unit07.synchronised1;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Bank {
-    static List<Account> accounts = new ArrayList<>();
+    volatile static List<Account> accounts = new ArrayList<>();
+    private static Pattern pattern = Pattern.compile("from: (\\d+) to: (\\d+) - (\\d+)");
 
     public static void transaction(Account a1, Account a2, int amount) {
-
         a1.deposit(amount);
         a2.withdraw(amount);
     }
 
     public static void transaction(String transactionInfo) {
 
-        Pattern p = Pattern.compile("from: (\\d+) to: (\\d+) - (\\d+)");
-        Matcher matcher = p.matcher(transactionInfo);
+        Matcher matcher = pattern.matcher(transactionInfo);
 
         if (matcher.find()) {
-            int fromID = Integer.valueOf(matcher.group(1));
-            int toID = Integer.valueOf(matcher.group(2));
-            int amount = Integer.valueOf(matcher.group(3));
-            Account fromAccount = accounts.get(fromID);
-            Account toAccount = accounts.get(toID);
-            transaction(fromAccount,toAccount,amount);
+            parseTransaction(matcher);
+            System.out.println(Thread.currentThread().getName());
         }
     }
+
+    public static void transaction(File transactionInfo) throws IOException, InterruptedException {
+        try (FileReader fileReader = new FileReader(transactionInfo);
+             Scanner scanner = new Scanner(fileReader)) {
+            List<String> lines = new ArrayList<>();
+            while (scanner.hasNextLine()) {
+                lines.add(scanner.nextLine());
+            }
+            lines.parallelStream().forEach(x -> transaction(x));
+        }
+    }
+
+
+    private static void parseTransaction(Matcher matcher) {
+        int fromID = Integer.valueOf(matcher.group(1));
+        int toID = Integer.valueOf(matcher.group(2));
+        int amount = Integer.valueOf(matcher.group(3));
+        Account fromAccount = accounts.get(fromID);
+        Account toAccount = accounts.get(toID);
+        transaction(fromAccount, toAccount, amount);
+    }
+
 }
--- unit07/src/test/java/com/epam/courses/javase/msid/unit07/synchronised1/BankTest.java
+++ unit07/src/test/java/com/epam/courses/javase/msid/unit07/synchronised1/BankTest.java
@@ -1,20 +1,46 @@
 package com.epam.courses.javase.msid.unit07.synchronised1;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import static org.junit.Assert.*;
 
 public class BankTest {
+    private Account a0;
+    private Account a1;
 
-    @Test
-    public void test() {
-        Account a0 = new SynchronisedAccount(100);
-        Account a1 = new SynchronisedAccount(100);
+    @Before
+    public void init(){
+
+        a0 = new SynchronisedAccount(100);
+        a1 = new SynchronisedAccount(100);
+
         Bank.accounts.add(a0);
         Bank.accounts.add(a1);
+    }
+
+    @Test
+    public void testWorkingRegexTransaction() {
+
         Bank.transaction("from: 0 to: 1 - 100");
+
         assertEquals(0, a0.getBalance());
         assertEquals(200, a1.getBalance());
     }
+
+    @Test
+    public void testMultithreadedOperations() throws IOException, InterruptedException {
+
+        File transactions = new File("transactions.txt");
+        Bank.transaction(transactions);
+
+        assertEquals(a1.getBalance(), a0.getBalance());
+
+    }
+
 
 }
